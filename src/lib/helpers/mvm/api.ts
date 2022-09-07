17726274783c23a5eabbc962c0e290154ed3b8b8
YeungKC <I@YeungKC.com>
2022-09-07 14:48:48 +0800

    improve mvm api

--- src/lib/helpers/mvm/api.ts
+++ src/lib/helpers/mvm/api.ts
@@ -2,8 +2,8 @@ export const getMvmTokens = async (address: string) => {
 	const response = await fetch(
 		`https://scan.mvm.dev/api?module=account&action=tokenlist&address=${address}`
 	);
-	const { result } = await response.json();
-	if (!result) throw new Error('No result');
+	const { result, status } = await response.json();
+	if (!status) throw new Error('No result');
 	return result as {
 		balance: string;
 		contractAddress: string;
