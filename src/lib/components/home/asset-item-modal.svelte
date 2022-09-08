d0c35a912cefa1d78acc2aa5889a901fd856a9ba
YeungKC <I@YeungKC.com>
2022-09-09 07:35:41 +0800

    fix

--- src/lib/components/home/asset-item-modal.svelte
+++ src/lib/components/home/asset-item-modal.svelte
@@ -3,7 +3,7 @@
 	import Close from '$lib/assets/close.svg?component';
 	import AssetIcon from '../base/asset-icon.svelte';
 	import LayoutBottomSheet from '../base/modal/layout-bottom-sheet.svelte';
-	import { ASSET_KEY, MODE_KEY } from '../../../routes/index@drawer.svelte';
+	import { ASSET_KEY, MODE_KEY } from './export';
 
 	export let onClose = () => {
 		//
