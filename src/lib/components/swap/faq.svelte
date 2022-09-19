bbd29ac2dd77a2f0eeadbf7a24c478f60ec935ba
YeungKC <I@YeungKC.com>
2022-09-19 19:44:49 +0800

    fix faq

--- src/lib/components/swap/faq.svelte
+++ src/lib/components/swap/faq.svelte
@@ -8,14 +8,16 @@
 	<p class=" mb-6 text-xl font-bold">How does it work?</p>
 	<div class=" space-y-6 text-sm font-medium text-black text-opacity-50">
 		<p>
-			MVM Bridge provides a cross-chain Convert service through 4swap and MixPay, which delivers
+			MVM Bridge provides a cross-chain convert service through 4swap and MixPay, which delivers
 			lower fees, faster transaction speed, and better trading liquidity.
 		</p>
 		<div>
 			<p class="text-base font-semibold text-black">4swap</p>
 			<p>
 				A decentralized protocol for automated liquidity provision offers over 140 pairs with a TVL
-				of over $100M. Please visit https://4swap.org for additional information.
+				of over $100M. Please visit <a href="https://4swap.org/" class="text-brand-primary"
+					>https://4swap.org/</a
+				> for additional information.
 			</p>
 		</div>
 
@@ -24,7 +26,7 @@
 			<p>
 				A decentralized Web3 payment protocol connects the most liquid exchanges, such as Binance,
 				Huobi, and Gate, to convert almost any cryptocurrency. For more info, please visit
-				https://mixpay.me.
+				<a href="https://mixpay.me/" class="text-brand-primary">https://mixpay.me/</a>.
 			</p>
 		</div>
 	</div>
