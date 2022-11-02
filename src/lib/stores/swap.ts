c662559dd5f739da7cd17d1f1e511ee856c01672
hundredark <hundredark@gmail.com>
2022-11-02 18:36:28 +0800

    Dont toast no pair in store

--- src/lib/stores/swap.ts
+++ src/lib/stores/swap.ts
@@ -37,7 +37,7 @@ const createSwapOrder = () => {
 
 		if (source === 'NoPair') {
 			set(undefined);
-			throw new Error('No Swap pair');
+      return;
 		}
 
 		if (mixpayOrderInfoUpdateTimer) clearInterval(mixpayOrderInfoUpdateTimer);
