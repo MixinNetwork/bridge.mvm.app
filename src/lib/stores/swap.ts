b67afd63834b26b012962013acb5d3ae71c459c6
hundredark <hundredark@gmail.com>
2022-11-02 18:39:09 +0800

    Fix format

--- src/lib/stores/swap.ts
+++ src/lib/stores/swap.ts
@@ -37,7 +37,7 @@ const createSwapOrder = () => {
 
 		if (source === 'NoPair') {
 			set(undefined);
-      return;
+			return;
 		}
 
 		if (mixpayOrderInfoUpdateTimer) clearInterval(mixpayOrderInfoUpdateTimer);
