b995e99aa8ee71d84902198932a567164f410abf
hundredark <hundredark@gmail.com>
2023-03-07 13:42:49 +0800

    Thorw error when failed to fetch withdrawal fee

--- src/lib/stores/model.ts
+++ src/lib/stores/model.ts
@@ -118,10 +118,13 @@ export const AssetWithdrawalFee = mapTemplate(
 				const { fetchWithdrawalFee } = await import('../helpers/api');
 				let fee = await fetchWithdrawalFee(asset_id, destination, tag);
 
-				if (!fee || Number(fee) === 0 || asset_id === chain_id) return fee;
+				if (!fee) throw new Error('Failed to fetch withdrawal fee');
+				if (Number(fee) === 0 || asset_id === chain_id) return fee;
 
 				const { fetchFeeOnAsset } = await import('../helpers/api');
 				fee = await fetchFeeOnAsset(asset_id, chain_id, fee);
+				if (!fee) throw new Error('Failed to fetch withdrawal fee');
+				
 				return fee;
 			},
 			{
