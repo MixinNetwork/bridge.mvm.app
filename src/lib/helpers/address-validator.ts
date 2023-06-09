7b5011aed8872e5bbc8e5acf28ad26208c8713f0
YeungKC <I@YeungKC.com>
2023-06-09 23:39:14 +0900

    refactor: Remove XMR address validation.

--- src/lib/helpers/address-validator.ts
+++ src/lib/helpers/address-validator.ts
@@ -1,14 +1,12 @@
 import {
 	BNB_1_ASSET_ID,
 	BNB_2_ASSET_ID,
-	BTC_ASSET_ID,
 	DOGE_ASSET_ID,
 	EOS_ASSET_ID,
 	ETH_ASSET_ID,
 	MATIC_ASSET_ID,
 	MOB_ASSET_ID,
-	TRX_ASSET_ID,
-	XMR_ASSET_ID
+	TRX_ASSET_ID
 } from '../constants/common';
 
 const cryptoRegExps: {
@@ -19,7 +17,6 @@ const cryptoRegExps: {
 	[EOS_ASSET_ID]: /^[.1-5a-z]{1,12}$/,
 	[DOGE_ASSET_ID]: /^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$/,
 	[TRX_ASSET_ID]: /^T[a-zA-Z1-9]{33}$/,
-	[XMR_ASSET_ID]: /^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$/,
 	[MATIC_ASSET_ID]: /^0x[a-fA-F0-9]{40}$/,
 	[BNB_1_ASSET_ID]: /^bnb[a-z0-9]{39}$/,
 	[BNB_2_ASSET_ID]: /^0x[a-fA-F0-9]{40}$/
